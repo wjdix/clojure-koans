16c1d646a34e4650b4c9c5bf8a85cf56baf871ba
William Dix <wjdix@northwestern.edu>
2011-04-21 09:31:27 -0500

    solutions for conditionals koans

--- src/koans/conditionals.clj
+++ src/koans/conditionals.clj
@@ -1,11 +1,11 @@
 (meditations
   "You will face many decisions"
-  (= __ (if (false? (= 4 5))
+  (= :a (if (false? (= 4 5))
             :a
             :b))
 
   "Some of them leave you no alternative"
-  (= __ (if (> 4 3)
+  (= [] (if (> 4 3)
             []))
 
   "And in such a case you may have nothing"
@@ -15,16 +15,16 @@
   "In others your alternative may be interesting"
   (= :glory (if (not (empty? ()))
                 :doom
-                __))
+                :glory))
 
   "You may have a multitude of possible paths"
   (let [x 5]
-    (= :your_road (cond (= x __) :road_not_taken
-                        (= x __) :another_road_not_taken
-                        :else __)))
+    (= :your_road (cond (= x 6) :road_not_taken
+                        (= x 4) :another_road_not_taken
+                        :else :your_road)))
 
   "Or your fate may be sealed"
-  (= __ (if-not (zero? __)
+  (= 'doom (if-not (zero? 0)
                 'doom
                 'doom)))
 
